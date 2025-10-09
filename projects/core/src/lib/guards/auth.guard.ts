4d3ac511ae3521245770387796e64958c91ec8d5
Aya Abdelmalek <ayamalekhasan@gmail.com>
2025-10-09 12:02:52 +0300

    update auth guard

--- projects/core/src/lib/guards/auth.guard.ts
+++ projects/core/src/lib/guards/auth.guard.ts
@@ -1,13 +1,17 @@
-import { inject } from "@angular/core";
-import { CanActivateFn } from "@angular/router";
-import { AuthService } from "../services";
-import { CORE_CONFIG, CoreConfig } from "../core-config";
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services';
+import { StorageHandler } from '../handlers/storage-handler';
+import { CookieService } from 'ngx-cookie-service';
+import { CORE_CONFIG, CoreConfig } from '../core-config';
 
 export const authGuard: CanActivateFn = () => {
   const appConfig = inject<CoreConfig>(CORE_CONFIG);
   const authService = inject(AuthService);
+  const cookieService = inject(CookieService);
   const token: string | null = authService.getUserToken();
 
+  StorageHandler.init(cookieService);
   if (!token) {
     window.location.href = appConfig.loginUrl;
     return false;
