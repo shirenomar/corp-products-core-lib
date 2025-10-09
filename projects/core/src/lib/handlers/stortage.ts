6bc82221e2d0fe9a9407510308cc53443b57f6fd
Aya Abdelmalek <ayamalekhasan@gmail.com>
2025-10-09 11:52:10 +0300

    handle cookies storage

--- projects/core/src/lib/handlers/stortage.ts
+++ projects/core/src/lib/handlers/stortage.ts
@@ -4,4 +4,10 @@ export enum LocalStorageKeys {
 }
 
 export enum SessionStorageKeys {}
-export type StorageEnum = SessionStorageKeys | LocalStorageKeys;
+export type StorageEnum = SessionStorageKeys | LocalStorageKeys | CookiesStorageKeys;
+
+export enum CookiesStorageKeys {
+  LANGUAGE = "LANGUAGE",
+  TOKEN = "TOKEN",
+  AUTHORIZATION = "Authorization"
+}
