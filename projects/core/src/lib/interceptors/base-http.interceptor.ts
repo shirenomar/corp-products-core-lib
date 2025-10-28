6aa4a6b408c859b32e5dabee53f89e847a34c8c8
Mohamed Ismael Abdelhady <mohabdelhady.c@stc.com.sa>
2025-10-28 15:54:52 +0300

    feat(APP_ERROR_HANDLER) : add APP_ERROR_HANDLER

--- projects/core/src/lib/interceptors/base-http.interceptor.ts
+++ projects/core/src/lib/interceptors/base-http.interceptor.ts
@@ -1,15 +1,17 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Inject, inject, Injectable } from '@angular/core';
-import { finalize, Observable, tap } from 'rxjs';
-import { IS_SYSTEM_LOADER, USE_API_PREFIX } from '../handlers/http-context-handler';
+import { catchError, finalize, Observable, switchMap, tap, throwError } from 'rxjs';
+import { IS_SYSTEM_LOADER } from '../handlers/http-context-handler';
 import { LoaderService } from '../services/loader.service';
 import { CORE_CONFIG, CoreConfig } from '../core-config';
 import { REQUEST_MODIFIER } from '../request-modifier';
+import { APP_ERROR_HANDLER } from '../error-config';
 
 @Injectable()
 export class HttpBaseInterceptor implements HttpInterceptor {
@@ -17,23 +19,27 @@ export class HttpBaseInterceptor implements HttpInterceptor {
 
   constructor(
     @Inject(CORE_CONFIG) protected appConfig: CoreConfig,
-    @Inject(REQUEST_MODIFIER) protected requestModifier: (req: HttpRequest<any>) => HttpRequest<any>
+    @Inject(REQUEST_MODIFIER) protected requestModifier: (req: HttpRequest<any>) => HttpRequest<any>,
+    @Inject(APP_ERROR_HANDLER) private appErrorHandler: (error: any) => Observable<any>
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log('interceptor', this.appConfig);
     request = this.requestModifier(request);
-
     // Handle loader
     const IS_SYSTEM_LOADER_CHECK = request.context.get(IS_SYSTEM_LOADER);
     this.loaderService.setLoading(true, IS_SYSTEM_LOADER_CHECK, request.url);
-
     return next.handle(request).pipe(
-      // Handler Token
-      tap((event) => {
-        //  if (event instanceof HttpResponse && request.url.includes(User_Token)) {
-        // Set user token ..
-        //  }
+       catchError((err: HttpErrorResponse) => {
+        if (!this.appErrorHandler) return throwError(() => err);
+        return this.appErrorHandler(err).pipe(
+          switchMap((shouldRetry: boolean) => {
+            if (shouldRetry) {
+              return next.handle(request);
+            }
+            return throwError(() => err);
+          })
+        );
       }),
       finalize(() => this.loaderService.setLoading(false, IS_SYSTEM_LOADER_CHECK, request.url))
     );
