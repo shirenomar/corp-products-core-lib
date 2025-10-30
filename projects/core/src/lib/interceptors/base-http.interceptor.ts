89b3e1f777404ded0f3f4878460f8fa3adc130cb
Mohamed Ismael Abdelhady <mohabdelhady.c@stc.com.sa>
2025-10-30 14:33:38 +0300

    feature(APP_ERROR_HANDLER) : implement APP_ERROR_HANDLER

--- projects/core/src/lib/interceptors/base-http.interceptor.ts
+++ projects/core/src/lib/interceptors/base-http.interceptor.ts
@@ -6,42 +6,75 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Inject, inject, Injectable } from '@angular/core';
-import { catchError, finalize, Observable, switchMap, tap, throwError } from 'rxjs';
+import {
+  catchError,
+  finalize,
+  Observable,
+  switchMap,
+  throwError,
+  from,
+  mergeMap
+} from 'rxjs';
 import { IS_SYSTEM_LOADER } from '../handlers/http-context-handler';
 import { LoaderService } from '../services/loader.service';
 import { CORE_CONFIG, CoreConfig } from '../core-config';
 import { REQUEST_MODIFIER } from '../request-modifier';
 import { APP_ERROR_HANDLER } from '../error-config';
+import { ErrorCode } from '../enums/error-code.enum';
 
 @Injectable()
 export class HttpBaseInterceptor implements HttpInterceptor {
+  isRefreshTokenCalled = false;
+  pendingRequests: { req: HttpRequest<any>; next: HttpHandler }[] = [];
   loaderService = inject(LoaderService);
 
   constructor(
     @Inject(CORE_CONFIG) protected appConfig: CoreConfig,
-    @Inject(REQUEST_MODIFIER) protected requestModifier: (req: HttpRequest<any>) => HttpRequest<any>,
-    @Inject(APP_ERROR_HANDLER) private appErrorHandler: (error: any) => Observable<any>
-  ) {}
+    @Inject(REQUEST_MODIFIER)
+    protected requestModifier: (req: HttpRequest<any>) => HttpRequest<any>,
+    @Inject(APP_ERROR_HANDLER)
+    private appErrorHandler: (error: any) => Observable<any>
+  ) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     console.log('interceptor', this.appConfig);
     request = this.requestModifier(request);
-    // Handle loader
+
     const IS_SYSTEM_LOADER_CHECK = request.context.get(IS_SYSTEM_LOADER);
     this.loaderService.setLoading(true, IS_SYSTEM_LOADER_CHECK, request.url);
+
     return next.handle(request).pipe(
-       catchError((err: HttpErrorResponse) => {
-        if (!this.appErrorHandler) return throwError(() => err);
-        return this.appErrorHandler(err).pipe(
-          switchMap((shouldRetry: boolean) => {
-            if (shouldRetry) {
-              return next.handle(request);
-            }
-            return throwError(() => err);
-          })
-        );
+      catchError((err: HttpErrorResponse) => {
+        if (err?.error?.errors?.errorCode === ErrorCode.JWT_EXPIRED) {
+          this.pendingRequests.push({ req: request, next });
+        }
+        if (!this.isRefreshTokenCalled) {
+          this.isRefreshTokenCalled = true;
+          if (!this.appErrorHandler) return throwError(() => err);
+          return this.appErrorHandler(err).pipe(
+            switchMap((shouldRetry: boolean) => {
+              if (shouldRetry) {
+                this.isRefreshTokenCalled = false;
+                const queued = [...this.pendingRequests];
+                this.pendingRequests = [];
+                if (queued.length > 0) {
+                  return from(queued).pipe(
+                    mergeMap((p) => p.next.handle(p.req))
+                  );
+                }
+              }
+              return throwError(() => err);
+            })
+          );
+        }
+        return throwError(() => err);
       }),
-      finalize(() => this.loaderService.setLoading(false, IS_SYSTEM_LOADER_CHECK, request.url))
+      finalize(() =>
+        this.loaderService.setLoading(false, IS_SYSTEM_LOADER_CHECK, request.url)
+      )
     );
   }
 }
