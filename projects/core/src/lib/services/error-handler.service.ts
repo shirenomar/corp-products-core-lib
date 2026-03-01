d5c7d9c4852e18261ccb06d57fdfc08313fef30e
melsafty <melsafty.c@stc.com.sa>
2026-03-01 15:10:31 +0300

    style: standardize quotes and formatting in error-handler.service.ts

--- projects/core/src/lib/services/error-handler.service.ts
+++ projects/core/src/lib/services/error-handler.service.ts
@@ -1,15 +1,17 @@
-import { HttpErrorResponse } from "@angular/common/http";
-import { ErrorHandler, Injectable, Injector } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { TOASTER_SERVICE, ToasterInterface } from "../core-config";
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TOASTER_SERVICE, ToasterInterface } from '../core-config';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class ErrorHandlerService implements ErrorHandler {
   constructor(
     private injector: Injector,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private authService: AuthService,
   ) {}
 
   private get toasterService(): ToasterInterface {
@@ -17,34 +19,35 @@ export class ErrorHandlerService implements ErrorHandler {
   }
 
   handleError(error: unknown): void {
-    console.log("ErrorHandlerServicee", error);
+    console.log('ErrorHandlerServicee', error);
     if (error instanceof Error && !(error instanceof HttpErrorResponse)) {
       console.error(error);
       return;
     }
 
     if (error instanceof HttpErrorResponse) {
-      console.error("HTTP Error:", error);
+      console.error('HTTP Error:', error);
 
       switch (error?.status) {
         case 400:
-           this.showError(`codes.${error.error.errors.errorType}`);
-           break
+          this.showError(`codes.${error.error.errors.errorType}`);
+          break;
         case 401:
           //TODO : Check After Implement login
-          this.showError(`network_errors.${error.error.errors.errorType}`);
+          this.authService.clearAuth();
+          // this.showError(`network_errors.${error.error.errors.errorType}`);
           break;
         case 404:
           this.showError(`network_errors.${error.error.errors.errorType}`);
           break;
         case 403:
-          this.showError("forbidden.message");
+          this.showError('forbidden.message');
           break;
         case 500:
-          this.showError("validation_error.server");
+          this.showError('validation_error.server');
           break;
         default:
-          this.showError("validation_error.random");
+          this.showError('validation_error.random');
           break;
       }
     }
@@ -52,7 +55,7 @@ export class ErrorHandlerService implements ErrorHandler {
 
   private showError(messageKey: string): void {
     this.toasterService.error({
-      title: this.translateService.instant("error.title"),
+      title: this.translateService.instant('error.title'),
       message: this.translateService.instant(`error.${messageKey}`),
     });
   }
